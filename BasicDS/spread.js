3ef4bd222a5e19688452fa36ce9b5e08ce0fa727
“SHAON1028” <shaonkhairul@gmail.com>
2023-04-30 16:28:40 +0600

    spread.js

---
+++ BasicDS/spread.js
@@ -0,0 +1,8 @@
+function spreadOut() {
+    let fragment = ['to', 'code'];
+    let sentence; // Change this line
+    sentence = ['learning',...fragment,'is','fun']
+    return sentence;
+  }
+  
+  console.log(spreadOut());
