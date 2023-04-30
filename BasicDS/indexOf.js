ef9f54a0d37f506395b68d182533ac37d628c781
“SHAON1028” <shaonkhairul@gmail.com>
2023-04-30 17:04:23 +0600

    indexOfiteration.js

--- BasicDS/indexOf.js
+++ BasicDS/indexOf.js
@@ -1,14 +1,11 @@
 // indexOf(), that allows us to quickly and easily check for the presence of an element on an array. indexOf() takes an element as a parameter, and when called, it returns the position, or index, of that element, or -1 if the element does not exist on the array.
 
-
-
 function quickCheck(arr, elem) {
-    // Only change code below this line
- if(arr.indexOf(elem)==-1){
-    return false
- }
- else return true
-    // Only change code above this line
-  }
-  
-  console.log(quickCheck(['squash', 'onions', 'shallots'], 'onions'));
+  // Only change code below this line
+  if (arr.indexOf(elem) == -1) {
+    return false;
+  } else return true;
+  // Only change code above this line
+}
+
+console.log(quickCheck(["squash", "onions", "shallots"], "onions"));
