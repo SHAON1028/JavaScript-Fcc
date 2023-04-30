6dd7cde24f9d64953bb7711cb3aa3e5e477a1c71
“SHAON1028” <shaonkhairul@gmail.com>
2023-04-30 15:54:31 +0600

    Js

---
+++ RegEx/index.js
@@ -0,0 +1,10 @@
+// let password = "abc123";
+// let checkPass = /(?=\w{3,6})(?=\D*\d)/;
+// console.log(checkPass.test(password))
+
+const arr = [2, 4, 5, 1, 7, 5, 2, 1];
+// Only change code below this line
+arr.splice(3,4)
+arr.splice(0,1)
+// Only change code above this line
+console.log(arr);
